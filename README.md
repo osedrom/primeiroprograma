dc54f78d3637b896c16ed4d07bd3f646abe02322
osedrom <arquitectologo38@hotmail.com>
2021-03-20 07:55:02 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# primeiroprograma
+ 
---
+++ js/principal.js
@@ -0,0 +1,7 @@
+function liga() {
+	document.querySelector('#lampada').src='imagen/lampada-apagada.gif';
+
+	}
+	function desliga(){
+		document.querySelector('#lampada').src='imagen/lampada-acesa.gif';
+	}
